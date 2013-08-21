f67e404c6c48d32b8c81a23898f8b4f15db07797
brian d foy <bdfoy@cpan.org>
2013-08-21 05:19:31 +0000

    Import of BDFOY/Modulino-Demo-0.10_01 from CPAN.
    gitpan-cpan-distribution: Modulino-Demo
    gitpan-cpan-version:      0.10_01
    gitpan-cpan-path:         BDFOY/Modulino-Demo-0.10_01.tar.gz
    gitpan-cpan-author:       BDFOY
    gitpan-cpan-maturity:     developer

---
+++ lib/Modulino/Test.pm
@@ -0,0 +1,152 @@
+package Modulino::Test;
+use strict;
+use utf8;
+
+use v5.14.2;
+
+use warnings;
+
+use subs qw();
+use vars qw($VERSION);
+
+$VERSION = '0.10_01';
+
+UNITCHECK {
+sub _running_under_tester {
+	!! $ENV{CPANTEST}
+	}
+
+sub _running_as_app {
+	defined scalar caller
+	}
+
+my $method = do {
+	   if( _running_under_tester()    ) { 'test' } # testing
+	elsif( _running_as_app()          ) { 'run'  } # running the application
+	else                                { undef  } # everything else
+	};
+
+__PACKAGE__->$method(@ARGV) if defined $method;
+}
+
+=encoding utf8
+
+=head1 NAME
+
+__PACKAGE__ - A demonstration of module ideas
+
+=head1 SYNOPSIS
+
+	use __PACKAGE__;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item run
+
+=cut
+
+sub run {
+	say "Running as program";
+	}
+
+sub _test_run {
+	require Test::More;
+
+	Test::More::pass();
+	Test::More::pass();
+
+	SKIP: {
+		Test::More::skip( "These tests don't work", 2 );
+		Test::More::fail();
+		Test::More::fail();
+		}
+	}
+
+=back
+
+=head2 Testing
+
+=over 4
+
+=item test
+
+Run all of the subroutines that start with C<_test_>. Each subroutine
+is wrapped in a C<Test::More> subtest.
+
+=cut
+
+sub test {
+	say "Running as test";
+
+	my( $class ) = @_;
+	my @tests = $class->_get_tests;
+
+	require Test::More;
+
+	foreach my $test ( @tests ) {
+		Test::More::subtest( $test => sub {
+			my $rc = eval { $class->$test(); 1 };
+			Test::More::diag( $@ ) unless defined $rc;
+			} );
+		}
+
+	Test::More::done_testing();
+	}
+
+sub _get_tests {
+	my( $class ) = @_;
+	no strict 'refs';
+	my $stub = $class . '::';
+	my @tests =
+		grep { defined &{"$stub$_"}    }
+		grep { 0 == index $_, '_test_' }
+		keys %{ "$stub" };
+
+	say "Tests are @tests";
+	@tests;
+	}
+
+=pod
+
+sub _test_doc {
+	require Test::More;
+	require Test::Pod;
+	require Test::Pod::Coverage;
+	our $TODO;
+
+	Test::Pod::pod_file_ok( __FILE__ );
+	TODO: {
+		local $TODO = "Pod::Coverage can't find the pod";
+		Test::Pod::Coverage::pod_coverage_ok( __PACKAGE__ );
+		}
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/modulino-demo/
+
+=head1 AUTHOR
+
+brian d foy, C<< <bdfoy@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2013, brian d foy, All Rights Reserved.
+
+You may redistribute this under the same terms as Perl itself.
+
+=cut
+
+1;
